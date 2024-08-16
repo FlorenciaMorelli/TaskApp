f68e2d433bb04a2eab582e2a5ac907b8b22a1b0e
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-08-16 20:10:14 -0300

    initComponent on MainActivity

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskapp.adapter.TaskAdapter
+import com.example.taskapp.data.Task
 import com.example.taskapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var recycler: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val tasks = TaskManager.getTasks()
+        initComponent(tasks)
 
+    }
+
+    fun initComponent(tasks: List<Task>){
+        recycler = binding.recyclerView
+        recycler.adapter = TaskAdapter(tasks)
+        recycler.layoutManager = LinearLayoutManager(this)
     }
 }
